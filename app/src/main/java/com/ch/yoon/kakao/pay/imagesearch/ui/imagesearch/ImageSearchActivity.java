fca9dc874d563f34fa5a9558963671fdf7da3d73
ch-yoon <chanhyuck1021@naver.com>
2019-08-01 14:46:31 +0900

    MainActivity -> ImageSearchActivity로 변경

--- app/src/main/java/com/ch/yoon/kakao/pay/imagesearch/ui/imagesearch/ImageSearchActivity.java
+++ app/src/main/java/com/ch/yoon/kakao/pay/imagesearch/ui/imagesearch/ImageSearchActivity.java
@@ -1,15 +1,15 @@
-package com.ch.yoon.kakao.pay.imagesearch.ui;
+package com.ch.yoon.kakao.pay.imagesearch.ui.imagesearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ch.yoon.kakao.pay.imagesearch.R;
 
-public class MainActivity extends AppCompatActivity {
+public class ImageSearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_image_search);
     }
 }
