71fb7b0dd6099c01ff33e20bd5e9c1810148e25a
ch-yoon <chanhyuck1021@naver.com>
2019-11-12 23:49:47 +0900

    ImageDocumentEntity 생성

--- app/src/main/java/com/ch/yoon/imagesearch/data/local/room/AppDatabase.kt
+++ app/src/main/java/com/ch/yoon/imagesearch/data/local/room/AppDatabase.kt
@@ -3,13 +3,14 @@ package com.ch.yoon.imagesearch.data.local.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.ch.yoon.imagesearch.data.local.room.dao.SearchLogDAO
+import com.ch.yoon.imagesearch.data.local.room.entity.ImageDocumentEntity
 import com.ch.yoon.imagesearch.data.local.room.entity.SearchLogEntity
 
 /**
  * Creator : ch-yoon
  * Date : 2019-10-28
  **/
-@Database(entities = [SearchLogEntity::class], version = 1, exportSchema = false)
+@Database(entities = [SearchLogEntity::class, ImageDocumentEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun searchLogDao(): SearchLogDAO
---
+++ app/src/main/java/com/ch/yoon/imagesearch/data/local/room/entity/ImageDocumentEntity.kt
@@ -0,0 +1,21 @@
+package com.ch.yoon.imagesearch.data.local.room.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Creator : ch-yoon
+ * Date : 2019-11-12
+ **/
+@Entity(tableName = "imageDocument")
+data class ImageDocumentEntity(
+    @PrimaryKey val id: String,
+    val collection: String,
+    val thumbnailUrl: String,
+    val imageUrl: String,
+    val width: Int,
+    val height: Int,
+    val displaySiteName: String,
+    val docUrl: String,
+    val dateTime: String
+)
