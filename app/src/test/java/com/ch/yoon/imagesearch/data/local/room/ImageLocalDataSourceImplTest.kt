4b4549ba1125d3fda6761232e28e6f610e442d59
ch-Yoon <chanhyuck1021@naver.com>
2019-11-14 18:29:45 +0900

    ImageLocalDataSource 테스트 추가

---
+++ app/src/test/java/com/ch/yoon/imagesearch/data/local/room/ImageLocalDataSourceImplTest.kt
@@ -0,0 +1,24 @@
+package com.ch.yoon.imagesearch.data.local.room
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Creator : ch-yoon
+ * Date : 2019-11-14
+ */
+class ImageLocalDataSourceImplTest {
+
+    @Test
+    fun saveFavoriteImageDocument() {
+    }
+
+    @Test
+    fun deleteFavoriteImageDocument() {
+    }
+
+    @Test
+    fun selectAllFavoriteImageDocumentList() {
+    }
+}
