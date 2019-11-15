1e4a1c10309f1cd23a9040a2dfba1cdbd8beb536
ch-yoon <chanhyuck1021@naver.com>
2019-11-16 00:40:39 +0900

    SearchListViewModel test 추가

--- app/src/test/java/com/ch/yoon/imagesearch/presentation/imagesearch/searchlist/SearchListViewModelTest.kt
+++ app/src/test/java/com/ch/yoon/imagesearch/presentation/imagesearch/searchlist/SearchListViewModelTest.kt
@@ -14,7 +14,6 @@ import com.ch.yoon.imagesearch.data.repository.image.model.ImageSearchResponse
 import com.ch.yoon.imagesearch.presentation.common.pageload.PageLoadHelper
 import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.subjects.PublishSubject
 import junit.framework.Assert.assertEquals
@@ -83,7 +82,7 @@ class SearchListViewModelTest {
     }
 
     @Test
-    fun `한줄에_표현되는_아이템의_갯수를_변경하면_반영되는지_테스트`() {
+    fun `한줄에 표현되는 아이템의 갯수를 변경하면 반영되는지_테스트`() {
         // when
         searchListViewModel.changeCountOfItemInLine(2)
         searchListViewModel.changeCountOfItemInLine(4)
@@ -97,7 +96,7 @@ class SearchListViewModelTest {
     @Test
     fun `최초 이미지 목록 요청시 수신한 이미지 목록이 반영되는지 테스트`() {
         // given
-        val expect = createVirtualImageSearchResponse(3, true)
+        val expect = createImageSearchResponse(3, true)
         every { mockRepository.requestImageList(any()) } returns (Single.just(expect))
         every { mockPageLoadHelper.requestFirstLoad(any()) } answers { capturedPageLoadApprove?.invoke("", 1, 1, false) }
 
@@ -113,7 +112,7 @@ class SearchListViewModelTest {
     @Test
     fun `최초 이미지 목록 요청시 수신한 리스트의 사이즈가 0이어도 반영되는지 테스트`() {
         // given
-        val expect = createVirtualImageSearchResponse(0, true)
+        val expect = createImageSearchResponse(0, true)
         every { mockRepository.requestImageList(any()) } returns (Single.just(expect))
         every { mockPageLoadHelper.requestFirstLoad(any()) } answers { capturedPageLoadApprove?.invoke("", 1, 1, false) }
 
@@ -129,7 +128,7 @@ class SearchListViewModelTest {
     @Test
     fun `최초 이미지 목록 요청시 수신한 리스트의 사이즈가 0이라면 검색결과가 없다는 메시지가 반영되는지 테스트`() {
         // given
-        val expect = createVirtualImageSearchResponse(0, true)
+        val expect = createImageSearchResponse(0, true)
         every { mockRepository.requestImageList(any()) } returns Single.just(expect)
         every { mockPageLoadHelper.requestFirstLoad(any()) } answers { capturedPageLoadApprove?.invoke("", 1, 1, false) }
 
@@ -160,7 +159,7 @@ class SearchListViewModelTest {
     @Test
     fun `마지막 데이터라면 마지막을 뜻하는 메시지가 반영되는지 테스트`() {
         // given
-        every { mockRepository.requestImageList(any()) } returns (Single.just(createVirtualImageSearchResponse(1, true)))
+        every { mockRepository.requestImageList(any()) } returns (Single.just(createImageSearchResponse(1, true)))
 
         // when
         searchListViewModel.loadImageList("테스트");
@@ -174,14 +173,14 @@ class SearchListViewModelTest {
     @Test
     fun `추가 이미지 목록 요청시 수신된 추가 이미지 목록이 반영되는지 테스트`() {
         // given
-        var isFirst = true
-        val firstValue = createVirtualImageSearchResponse(3, false)
-        val secondValue = createVirtualImageSearchResponse(3, true)
+        val firstValue = createImageSearchResponse(3, false)
+        val secondValue = createImageSearchResponse(3, true)
         val expectedList = mutableListOf<ImageDocument>().apply {
             addAll(firstValue.imageDocumentList)
             addAll(secondValue.imageDocumentList)
         }
 
+        var isFirst = true
         every { mockRepository.requestImageList(any()) } returns if(isFirst) {
             isFirst = false
             Single.just(firstValue)
@@ -205,7 +204,7 @@ class SearchListViewModelTest {
     @Test
     fun `추가 이미지 목록 요청시 마지막 데이터였다면 레파지토리에 요청을 안하는지 테스트`() {
         // given
-        every { mockRepository.requestImageList(any()) } returns (Single.just(createVirtualImageSearchResponse(1, true)))
+        every { mockRepository.requestImageList(any()) } returns (Single.just(createImageSearchResponse(1, true)))
         every { mockPageLoadHelper.requestFirstLoad(any()) } answers { capturedPageLoadApprove?.invoke("", 1, 1, true) }
         every { mockPageLoadHelper.requestPreloadIfPossible(any(), any(), any()) } answers { capturedPageLoadApprove?.invoke("", 1, 1, false) }
 
@@ -222,7 +221,7 @@ class SearchListViewModelTest {
     @Test
     fun `재시도 요청시 레파지토리에 요청을 하는지 테스트`() {
         // given
-        every { mockRepository.requestImageList(any()) } returns (Single.just(createVirtualImageSearchResponse(1, true)))
+        every { mockRepository.requestImageList(any()) } returns (Single.just(createImageSearchResponse(1, true)))
         every { mockPageLoadHelper.requestRetryAsPreviousValue() } answers { capturedPageLoadApprove?.invoke("", 1, 1, false) }
 
         // when
@@ -235,7 +234,7 @@ class SearchListViewModelTest {
     @Test
     fun `이미지 검색 타입 변경시 반영되는지 테스트`() {
         // given
-        every { mockRepository.requestImageList(any()) } returns (Single.just(createVirtualImageSearchResponse(1, true)))
+        every { mockRepository.requestImageList(any()) } returns (Single.just(createImageSearchResponse(1, true)))
         every { mockPageLoadHelper.requestRetryAsPreviousValue() } answers { capturedPageLoadApprove?.invoke("", 1, 1, false) }
 
         // when
@@ -255,7 +254,7 @@ class SearchListViewModelTest {
     @Test
     fun `이미지 클릭 시 이미지 상세 화면 이동 이벤트 호출되는지 테스트`() {
         // given
-        val imageDocument = createVirtualImageDocument(0)
+        val imageDocument = createImageDocument("0")
 
         // when
         searchListViewModel.onClickImage(imageDocument)
@@ -266,26 +265,90 @@ class SearchListViewModelTest {
         }
     }
 
-    private fun createVirtualImageSearchResponse(
+    @Test
+    fun `새로운 좋아요 이벤트 수신시 기존 목록에 반영되는지 테스트`() {
+        // given
+        every {
+            mockPageLoadHelper.requestFirstLoad(any())
+        } answers {
+            capturedPageLoadApprove?.invoke("", 1, 1, true)
+        }
+
+        val imageResponse = createImageSearchResponse(3, true)
+        imageResponse.imageDocumentList.forEach { it.isFavorite = false }
+        every { mockRepository.requestImageList(any()) } returns Single.just(imageResponse)
+
+        val subject = PublishSubject.create<ImageDocument>()
+        every { mockRepository.observeChangingFavoriteImage() } returns subject
+
+        // when
+        searchListViewModel.loadImageList("테스트")
+        searchListViewModel.observeChangingFavoriteImage()
+
+        val noFavoriteImage = createImageDocument(imageResponse.imageDocumentList[0].id, true)
+        subject.onNext(noFavoriteImage)
+
+        // then
+        val expected = createImageSearchResponse(3, true).imageDocumentList.toMutableList()
+        expected.forEach { it.isFavorite = false }
+        expected[0].isFavorite = true
+        searchListViewModel.imageDocumentList.observeForever {
+            assertEquals(expected.toList(), it)
+        }
+    }
+
+    @Test
+    fun `새로운 좋아요 취소 이벤트 수신시 기존 목록에 반영되는지 테스트`() {
+        // given
+        every {
+            mockPageLoadHelper.requestFirstLoad(any())
+        } answers {
+            capturedPageLoadApprove?.invoke("", 1, 1, true)
+        }
+
+        val imageResponse = createImageSearchResponse(3, true)
+        imageResponse.imageDocumentList.forEach { it.isFavorite = true }
+        every { mockRepository.requestImageList(any()) } returns Single.just(imageResponse)
+
+        val subject = PublishSubject.create<ImageDocument>()
+        every { mockRepository.observeChangingFavoriteImage() } returns subject
+
+        // when
+        searchListViewModel.loadImageList("테스트")
+        searchListViewModel.observeChangingFavoriteImage()
+
+        val noFavoriteImage = createImageDocument(imageResponse.imageDocumentList[0].id, false)
+        subject.onNext(noFavoriteImage)
+
+        // then
+        val expected = createImageSearchResponse(3, true).imageDocumentList.toMutableList()
+        expected.forEach { it.isFavorite = true }
+        expected[0].isFavorite = false
+        searchListViewModel.imageDocumentList.observeForever {
+            assertEquals(expected.toList(), it)
+        }
+    }
+
+    private fun createImageSearchResponse(
         documentListSize: Int,
         isLastData: Boolean
     ): ImageSearchResponse {
         val searchMeta = ImageSearchMeta(isLastData)
-        val imageDocumentList = createVirtualImageDocumentList(documentListSize)
+        val imageDocumentList = createImageDocumentList(documentListSize)
         return ImageSearchResponse(searchMeta, imageDocumentList)
     }
 
-    private fun createVirtualImageDocumentList(size: Int): MutableList<ImageDocument> {
+    private fun createImageDocumentList(size: Int): MutableList<ImageDocument> {
         val list = mutableListOf<ImageDocument>()
         for(i in 0 until size) {
-            list.add(createVirtualImageDocument(i))
+            list.add(createImageDocument(i.toString()))
         }
         return list
     }
 
-    private fun createVirtualImageDocument(id: Int): ImageDocument {
+    private fun createImageDocument(id: String, isFavorite: Boolean = false): ImageDocument {
         return ImageDocument(
-            "id$id",
+            id,
             "collection$id",
             "thumbnailUrl$id",
             "imageUrlInfo$id",
@@ -294,7 +357,7 @@ class SearchListViewModelTest {
             "displaySiteName$id",
             "docUrl$id",
             "dateTime$id",
-            false
+            isFavorite
         )
     }
 
